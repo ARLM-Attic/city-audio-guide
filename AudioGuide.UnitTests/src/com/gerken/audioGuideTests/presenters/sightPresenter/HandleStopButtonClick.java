f6638f5949400467c30cf91d2550b42d2059cc1f
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-04-03 14:53:51 +0000

    Created test
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@25090 3b72ae23-dab2-410c-8069-39ac8a3268c2

---
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/sightPresenter/HandleStopButtonClick.java
@@ -0,0 +1,42 @@
+package com.gerken.audioGuideTests.presenters.sightPresenter;
+
+import static org.mockito.Mockito.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Test;
+
+import com.gerken.audioGuide.interfaces.*;
+import com.gerken.audioGuide.interfaces.views.SightView;
+import com.gerken.audioGuide.objectModel.*;
+import com.gerken.audioGuide.presenters.SightPresenter;;
+
+public class HandleStopButtonClick {
+	
+	@Test
+	public void Given_PlayerHasBeenPlaying__Then_PlayerGetsStopped() throws Exception {
+		final String WHATEVER_STRING = "whatever";		
+		
+		City city = new City(1, "Default", WHATEVER_STRING);
+		SightView view = mock(SightView.class);
+		AudioPlayer player = mock(AudioPlayer.class);
+		when(player.isPlaying()).thenReturn(true);
+		
+		SightPresenter sut = CreateSut(city, view, player);
+		
+		// --- Act
+		sut.handleStopButtonClick();
+		
+		// --- Assert
+		verify(player).stop();
+	}
+
+	private SightPresenter CreateSut(City city, SightView view, AudioPlayer player) {
+		AssetStreamProvider assetStreamProvider = mock(AssetStreamProvider.class);
+		SharedPreferenceStorage prefStorage = mock(SharedPreferenceStorage.class);
+		Logger logger = mock(Logger.class);
+		
+		return new SightPresenter(city, view, assetStreamProvider,
+				player, prefStorage, logger);
+	}
+}
