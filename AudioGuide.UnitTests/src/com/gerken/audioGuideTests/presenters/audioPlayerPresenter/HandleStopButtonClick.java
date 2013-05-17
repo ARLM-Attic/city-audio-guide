70f9c429530d3eb6c26e2d62d1cce23d0c8dcf57
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-05-17 15:37:02 +0000

    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@26348 3b72ae23-dab2-410c-8069-39ac8a3268c2

---
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/audioPlayerPresenter/HandleStopButtonClick.java
@@ -0,0 +1,54 @@
+package com.gerken.audioGuideTests.presenters.audioPlayerPresenter;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.gerken.audioGuide.interfaces.AudioPlayer;
+import com.gerken.audioGuide.interfaces.Logger;
+import com.gerken.audioGuide.interfaces.OnEventListener;
+import com.gerken.audioGuide.interfaces.views.AudioPlayerView;
+import com.gerken.audioGuide.presenters.AudioPlayerPresenter;
+
+
+public class HandleStopButtonClick {
+	
+	@Test
+	public void Given_PlayerHasBeenPlaying__Then_PlayerGetsStopped() throws Exception {
+		
+		AudioPlayerView playerView = mock(AudioPlayerView.class);
+		AudioPlayer player = mock(AudioPlayer.class);
+		when(player.isPlaying()).thenReturn(true);
+		
+		SutSetupResult sutSetupResult = setupSut(playerView, player);
+		
+		// --- Act
+		sutSetupResult.stopButtonPressedListener.onEvent();
+		
+		// --- Assert
+		verify(player).stop();
+	}
+	
+	private SutSetupResult setupSut(AudioPlayerView playerView, AudioPlayer audioPlayer) {
+		SutSetupResult result = new SutSetupResult();
+
+		ArgumentCaptor<OnEventListener> stopButtonPressedListenerCaptor = 
+				ArgumentCaptor.forClass(OnEventListener.class);
+		doNothing().when(playerView).addStopPressedListener(stopButtonPressedListenerCaptor.capture());
+		
+		
+		AudioPlayerPresenter sut = new AudioPlayerPresenter(playerView, audioPlayer, mock(Logger.class));
+		
+		result.sut = sut;
+		result.stopButtonPressedListener = stopButtonPressedListenerCaptor.getValue();
+		
+		return result;
+	}
+	
+	private class SutSetupResult {
+		public AudioPlayerPresenter sut;
+		public OnEventListener stopButtonPressedListener;
+	}
+
+}
