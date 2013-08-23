64371dd66a85f0d6a3f7bca492335142f9639184
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-08-23 16:00:33 +0000

    Logging unhandled exceptions
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@28040 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide/src/com/gerken/audioGuide/GuideApplication.java
+++ AudioGuide/src/com/gerken/audioGuide/GuideApplication.java
@@ -1,5 +1,7 @@
 package com.gerken.audioGuide;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
 import com.gerken.audioGuide.objectModel.City;
 import com.gerken.audioGuide.presenters.AudioPlayerPresenter;
 import com.gerken.audioGuide.presenters.SightPresenter;
@@ -8,12 +10,26 @@ import com.gerken.audioGuide.services.GuideDataManager;
 import android.app.Application;
 import android.util.Log;
 import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 import de.mindpipe.android.logging.log4j.LogConfigurator;
 
 public class GuideApplication extends Application {
 	private final String LOG_TAG="AudioGuide";
 	
 	private City _city = null;
+	private Logger _log;
+	
+	private UncaughtExceptionHandler _defaultCrashHandler;
+	
+	private UncaughtExceptionHandler _crashHandler = new UncaughtExceptionHandler() {
+		
+		@Override
+		public void uncaughtException(Thread thread, Throwable ex) {
+			_log.fatal("Unhandled exception occurred", ex);
+			_defaultCrashHandler.uncaughtException(thread, ex);
+		}
+	};
 	
 	public City getCity() {
 		return _city;
@@ -29,6 +45,9 @@ public class GuideApplication extends Application {
 			mgr.init();
 			_city = mgr.getCity();
 			configureLog4J();
+			_log =  Logger.getLogger(GuideApplication.class);
+			_defaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
+			Thread.setDefaultUncaughtExceptionHandler(_crashHandler);
 		}
 		catch(Exception ex){
 			Log.e(LOG_TAG, "Guide initialization failed", ex);
@@ -47,4 +66,6 @@ public class GuideApplication extends Application {
         logConfigurator.setLevel(AudioPlayerPresenter.class.getName(), Level.ERROR);
         logConfigurator.configure();
 	}
+	
+
 }
