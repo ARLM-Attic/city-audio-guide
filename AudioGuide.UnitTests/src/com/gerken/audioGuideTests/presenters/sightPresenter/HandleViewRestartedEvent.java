ed3d8fad5da9f640e18a1e1e7b373443ebb9b1b7
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-08-27 14:42:00 +0000

    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@28126 3b72ae23-dab2-410c-8069-39ac8a3268c2

---
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/sightPresenter/HandleViewRestartedEvent.java
@@ -0,0 +1,62 @@
+package com.gerken.audioGuideTests.presenters.sightPresenter;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.gerken.audioGuide.interfaces.AudioPlayer;
+import com.gerken.audioGuide.interfaces.OnEventListener;
+import com.gerken.audioGuide.interfaces.views.AudioPlayerView;
+import com.gerken.audioGuide.interfaces.views.SightView;
+import com.gerken.audioGuide.presenters.SightPresenter;
+
+public class HandleViewRestartedEvent {
+	@Test
+	public void Given_AudioPlayerWasPausedOnViewStop__Then_AudioPlayerResumesPlaying() throws Exception {
+		final boolean PLAYER_IS_PLAYING = true;
+		
+		SightView sightView = mock(SightView.class);
+		AudioPlayer player = mock(AudioPlayer.class);
+		when(player.isPlaying()).thenReturn(PLAYER_IS_PLAYING);
+
+		SutSetupResult sutSetupResult = setupSut(sightView, player);
+		sutSetupResult.sightViewStoppedListener.onEvent();
+		
+		// --- Act
+		sutSetupResult.sightViewRestartedListener.onEvent();
+		
+		// --- Assert
+		verify(player).play();
+	}
+	
+	private SutSetupResult setupSut(SightView sightView, AudioPlayer player) {
+		SutSetupResult result = new SutSetupResult();
+		
+		ArgumentCaptor<OnEventListener> sightViewStoppedListenerCaptor = 
+				ArgumentCaptor.forClass(OnEventListener.class);
+		doNothing().when(sightView).addViewStoppedListener(sightViewStoppedListenerCaptor.capture());
+		
+		ArgumentCaptor<OnEventListener> sightViewRestartedListenerCaptor = 
+				ArgumentCaptor.forClass(OnEventListener.class);
+		doNothing().when(sightView).addViewRestartedListener(sightViewRestartedListenerCaptor.capture());
+		
+		SightPresenter sut = new SightPresenter(sightView, mock(AudioPlayerView.class));
+		sut.setAudioPlayer(player);
+		
+		result.sut = sut;
+		result.sightViewStoppedListener = sightViewStoppedListenerCaptor.getValue();
+		result.sightViewRestartedListener = sightViewRestartedListenerCaptor.getValue();
+		
+		return result;
+	}
+	
+	private class SutSetupResult {
+		public SightPresenter sut;
+		public OnEventListener sightViewStoppedListener;
+		public OnEventListener sightViewRestartedListener;
+	}
+}
---
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/sightPresenter/HandleViewStoppedEvent.java
@@ -0,0 +1,73 @@
+package com.gerken.audioGuideTests.presenters.sightPresenter;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.gerken.audioGuide.interfaces.AudioPlayer;
+import com.gerken.audioGuide.interfaces.OnEventListener;
+import com.gerken.audioGuide.interfaces.views.AudioPlayerView;
+import com.gerken.audioGuide.interfaces.views.SightView;
+import com.gerken.audioGuide.presenters.SightPresenter;
+
+public class HandleViewStoppedEvent {
+	@Test
+	public void Given_AudioPlayerIsPlaying__Then_AudioPlayerIsPaused() throws Exception {
+		final boolean PLAYER_IS_PLAYING = true;
+		
+		SightView sightView = mock(SightView.class);
+		AudioPlayer player = mock(AudioPlayer.class);
+		when(player.isPlaying()).thenReturn(PLAYER_IS_PLAYING);
+
+		SutSetupResult sutSetupResult = setupSut(sightView, player);
+		
+		// --- Act
+		sutSetupResult.sightViewStoppedListener.onEvent();
+		
+		// --- Assert
+		verify(player).pause();
+	}
+	
+	@Test
+	public void Given_AudioPlayerIsNotPlaying__Then_AudioPlayerIsNotPaused() throws Exception {
+		final boolean PLAYER_IS_NOT_PLAYING = false;
+		
+		SightView sightView = mock(SightView.class);
+		AudioPlayer player = mock(AudioPlayer.class);
+		when(player.isPlaying()).thenReturn(PLAYER_IS_NOT_PLAYING);
+
+		SutSetupResult sutSetupResult = setupSut(sightView, player);
+		
+		// --- Act
+		sutSetupResult.sightViewStoppedListener.onEvent();
+		
+		// --- Assert
+		verify(player, never()).pause();
+	}
+	
+	private SutSetupResult setupSut(SightView sightView, AudioPlayer player) {
+		SutSetupResult result = new SutSetupResult();
+		
+		ArgumentCaptor<OnEventListener> sightViewStoppedListenerCaptor = 
+				ArgumentCaptor.forClass(OnEventListener.class);
+		doNothing().when(sightView).addViewStoppedListener(sightViewStoppedListenerCaptor.capture());		
+		
+		SightPresenter sut = new SightPresenter(sightView, mock(AudioPlayerView.class));
+		sut.setAudioPlayer(player);
+		
+		result.sut = sut;
+		result.sightViewStoppedListener = sightViewStoppedListenerCaptor.getValue();
+		
+		return result;
+	}
+	
+	private class SutSetupResult {
+		public SightPresenter sut;
+		public OnEventListener sightViewStoppedListener;
+	}
+}
