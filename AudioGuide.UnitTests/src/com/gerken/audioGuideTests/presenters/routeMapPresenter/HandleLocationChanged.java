5ece0bf81b4b60c1ce547979cf9a353fbbf752ed
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-10-15 13:13:41 +0000

    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@28965 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/routeMapPresenter/HandleLocationChanged.java
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/routeMapPresenter/HandleLocationChanged.java
@@ -17,7 +17,7 @@ import com.gerken.audioGuide.presenters.RouteMapPresenter;
 public class HandleLocationChanged {
 	
 	@Test
-	public void Given_NewLocationIsOnMap__Then_MapPointerUpdated() {
+	public void Given_NewLocationIsOnMap__Then_MapPointerShown_MapScrolled() {
 		final double MAP_NORTH = 26.0;
 		final double MAP_WEST = 12.0;
 		final double MAP_SOUTH = 22.0;
@@ -28,10 +28,50 @@ public class HandleLocationChanged {
 		
 		final int MAP_WIDTH = 400;
 		final int MAP_HEIGHT = 200;
+		final int SCR_WIDTH = 100;
+		final int SCR_HEIGHT = 200;
 		
 		final int EXP_MAP_POINTER_X = 100;
 		final int EXP_MAP_POINTER_Y = 50;
+		final int EXP_SCROLL_TO_X = 50;
+		final int EXP_SCROLL_TO_Y = 0;
 		
+		final int ROUTE_ID = 42;
+		
+		MapBounds bounds = new MapBounds(MAP_NORTH, MAP_WEST, MAP_SOUTH, MAP_EAST);
+		City city = createSingleRouteCity(ROUTE_ID, bounds);
+		
+		RouteMapView view = mock(RouteMapView.class);
+		when(view.getHeight()).thenReturn(SCR_HEIGHT);
+		when(view.getWidth()).thenReturn(SCR_WIDTH);
+		when(view.getMapHeight()).thenReturn(MAP_HEIGHT);
+		when(view.getMapWidth()).thenReturn(MAP_WIDTH);
+		when(view.getRouteId()).thenReturn(ROUTE_ID);
+		LocationTracker tracker = mock(LocationTracker.class);
+		
+		SutSetupResult sutSetupResult = setupSut(city, view, tracker);
+		
+		// --- Act
+		sutSetupResult.locationChangedListener.onLocationChanged(NEW_LATITUDE, NEW_LONGITUDE);
+		
+		// --- Assert
+		verify(view).showLocationPointerAt(EXP_MAP_POINTER_X, EXP_MAP_POINTER_Y);
+		verify(view).scrollTo(EXP_SCROLL_TO_X, EXP_SCROLL_TO_Y);
+	}
+	
+	@Test
+	public void Given_NewLocationIsOutsideMap__Then_MapPointerHidden_MapNotScrolled() {
+		final double MAP_NORTH = 26.0;
+		final double MAP_WEST = 12.0;
+		final double MAP_SOUTH = 22.0;
+		final double MAP_EAST = 16.0;
+		
+		final double NEW_LATITUDE = 31.0;
+		final double NEW_LONGITUDE = 13.0;
+		
+		final int MAP_WIDTH = 400;
+		final int MAP_HEIGHT = 200;
+
 		final int ROUTE_ID = 42;
 		
 		MapBounds bounds = new MapBounds(MAP_NORTH, MAP_WEST, MAP_SOUTH, MAP_EAST);
@@ -49,7 +89,45 @@ public class HandleLocationChanged {
 		sutSetupResult.locationChangedListener.onLocationChanged(NEW_LATITUDE, NEW_LONGITUDE);
 		
 		// --- Assert
-		verify(view).setLocationPointerPosition(EXP_MAP_POINTER_X, EXP_MAP_POINTER_Y);
+		verify(view).hideLocationPointer();
+		verify(view, never()).scrollTo(anyInt(), anyInt());
+	}
+	
+	@Test
+	public void Given_TwiceNewLocationIsOnMap__Then_MapScrolledOnlyOnce() throws InterruptedException {
+		final double MAP_NORTH = 26.0;
+		final double MAP_WEST = 12.0;
+		final double MAP_SOUTH = 22.0;
+		final double MAP_EAST = 16.0;
+		
+		final double NEW_LATITUDE = 25.0;
+		final double NEW_LONGITUDE = 13.0;
+		
+		final int MAP_WIDTH = 400;
+		final int MAP_HEIGHT = 200;
+		
+		final int ROUTE_ID = 42;
+		
+		MapBounds bounds = new MapBounds(MAP_NORTH, MAP_WEST, MAP_SOUTH, MAP_EAST);
+		City city = createSingleRouteCity(ROUTE_ID, bounds);
+		
+		RouteMapView view = mock(RouteMapView.class);
+		when(view.getHeight()).thenReturn(42);
+		when(view.getWidth()).thenReturn(42);
+		when(view.getMapHeight()).thenReturn(MAP_HEIGHT);
+		when(view.getMapWidth()).thenReturn(MAP_WIDTH);
+		when(view.getRouteId()).thenReturn(ROUTE_ID);
+		LocationTracker tracker = mock(LocationTracker.class);
+		
+		SutSetupResult sutSetupResult = setupSut(city, view, tracker);
+		
+		// --- Act
+		sutSetupResult.locationChangedListener.onLocationChanged(NEW_LATITUDE, NEW_LONGITUDE);
+		Thread.sleep(100);
+		sutSetupResult.locationChangedListener.onLocationChanged(NEW_LATITUDE+0.1, NEW_LONGITUDE+0.1);
+		
+		// --- Assert
+		verify(view, times(1)).scrollTo(anyInt(), anyInt());
 	}
 	
 	private SutSetupResult setupSut(City city, RouteMapView view, LocationTracker tracker) {
