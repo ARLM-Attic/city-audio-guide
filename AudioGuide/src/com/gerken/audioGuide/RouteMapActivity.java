e56aa19399f1a6918e4498e6ab78ec34c4f69d28
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-10-24 13:30:09 +0000

    Animated map pointer
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@29142 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide/src/com/gerken/audioGuide/RouteMapActivity.java
+++ AudioGuide/src/com/gerken/audioGuide/RouteMapActivity.java
@@ -8,6 +8,8 @@ import com.gerken.audioGuide.util.IntentExtraManager;
 import android.os.Bundle;
 import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.AbsoluteLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
@@ -18,6 +20,7 @@ public class RouteMapActivity extends BasicGuideActivity implements RouteMapView
 	private View _rootView;	
 	private ImageView _mapImage;
 	private IntentExtraManager _intentExtraManager;	
+	private Animation _mapPointerAnimation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class RouteMapActivity extends BasicGuideActivity implements RouteMapView
 		_rootView = findViewById(R.id.rootLayout);
 		_mapImage = (ImageView)findViewById(R.id.mapImage);
 		_intentExtraManager = new IntentExtraManager(getIntent());
+		_mapPointerAnimation = createMapPointerAnimation();
 		
 		((GuideApplication)getApplication()).getPresenterContainer().initRouteMapPresenter(this);
 		
@@ -71,11 +75,12 @@ public class RouteMapActivity extends BasicGuideActivity implements RouteMapView
 	public void showLocationPointerAt(int x, int y) {
 		View mapPointer = findViewById(R.id.mapPointerImage);
 		AbsoluteLayout.LayoutParams pointerLp = (AbsoluteLayout.LayoutParams)mapPointer.getLayoutParams();
-		pointerLp.x = x;
-		pointerLp.y = y;
+		pointerLp.x = x - (int)(mapPointer.getWidth()/2);
+		pointerLp.y = y - (int)(mapPointer.getHeight()/2);
 		//pointerLp.setMargins(100, 200, pointerLp.rightMargin, pointerLp.bottomMargin);
 		mapPointer.setLayoutParams(pointerLp);
 		mapPointer.setVisibility(View.VISIBLE);
+        mapPointer.startAnimation(_mapPointerAnimation);
 	}
 
 	@Override
@@ -102,4 +107,12 @@ public class RouteMapActivity extends BasicGuideActivity implements RouteMapView
 		return _rootView;
 	}
 
+	private Animation createMapPointerAnimation() {
+		Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(1000);
+        //anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        return anim;
+	}
 }
