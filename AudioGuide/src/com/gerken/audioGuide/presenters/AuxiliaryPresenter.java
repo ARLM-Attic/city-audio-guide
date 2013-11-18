6e3301c0f46eb02d1020e9467505099360cac94c
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-11-18 15:37:51 +0000

    Loading portrait/landscape background images
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@29610 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide/src/com/gerken/audioGuide/presenters/AuxiliaryPresenter.java
+++ AudioGuide/src/com/gerken/audioGuide/presenters/AuxiliaryPresenter.java
@@ -8,11 +8,17 @@ import com.gerken.audioGuide.interfaces.views.AuxiliaryView;
 import com.gerken.audioGuide.objectModel.City;
 
 public class AuxiliaryPresenter {
+	private static final String DEFAULT_SUFFIX_PORT = "_port";
+	private static final String DEFAULT_SUFFIX_LAND = "_land";
+	
 	private AuxiliaryView _auxView;
 	private City _city;
 	
 	private DownscalingBitmapLoader _bitmapLoader;
 	private Logger _logger;
+	
+	private String _suffixPortrait = DEFAULT_SUFFIX_PORT;
+	private String _suffixLandscape = DEFAULT_SUFFIX_LAND;
 
 	private OnEventListener _viewLayoutCompleteListener = new OnEventListener() {		
 		@Override
@@ -38,8 +44,10 @@ public class AuxiliaryPresenter {
 	private void handleViewLayoutComplete(){
 		BitmapDownscalingResult backgroundImage = null;
 		try {
+			String img = getBackgroundImageFileName();			
+			logDebug("Loading " + img);
 			backgroundImage = _bitmapLoader.load(
-				_city.getHelpBackgroundImageName(),	_auxView.getWidth(), _auxView.getHeight());
+				img, _auxView.getWidth(), _auxView.getHeight());
 		}
 		catch(Exception ex) {
 			logError("Unable to set the background image", ex);
@@ -48,8 +56,24 @@ public class AuxiliaryPresenter {
 			_auxView.setBackgroundImage(backgroundImage);
 	}
 	
+	private String getBackgroundImageFileName() {
+		String suffix = (_auxView.getWidth() > _auxView.getHeight()) ? 
+				_suffixLandscape : _suffixPortrait;		
+		
+		String[] fileNameParts = _city.getHelpBackgroundImageName().split("\\.");
+		if(fileNameParts.length == 2) {
+			return fileNameParts[0] + suffix + "." + fileNameParts[1];
+		}
+		else
+			return _city.getHelpBackgroundImageName() + suffix;
+	}
+	
 	private void logError(String message, Throwable ex) {
 		if(_logger != null)
 			_logger.logError(message, ex);
+	}
+	private void logDebug(String message) {
+		if(_logger != null)
+			_logger.logDebug(message);
 	}
 }
