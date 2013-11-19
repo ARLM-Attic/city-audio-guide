9eb67f391c303b9cad3d26cfe2ed03c831a3d551
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-11-19 08:04:47 +0000

    Added test for AuxiliaryPresenter
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@29644 3b72ae23-dab2-410c-8069-39ac8a3268c2

---
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/auxiliaryPresenter/HandleViewLayoutComplete.java
@@ -0,0 +1,70 @@
+package com.gerken.audioGuideTests.presenters.auxiliaryPresenter;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import com.gerken.audioGuide.interfaces.DownscalingBitmapLoader;
+import com.gerken.audioGuide.interfaces.OnEventListener;
+import com.gerken.audioGuide.interfaces.views.AuxiliaryView;
+import com.gerken.audioGuide.objectModel.City;
+import com.gerken.audioGuide.objectModel.CityConfiguration;
+import com.gerken.audioGuide.presenters.AuxiliaryPresenter;
+
+public class HandleViewLayoutComplete {
+	private Random _random = new Random(System.currentTimeMillis());
+	
+	@Test
+	public void Given_ViewIsLandscape__Then_LandscapeBackgroundIsLoaded() throws Exception {
+		final int WIDTH = 320;
+		final int HEIGHT = 240;
+		final String EXPECTED_LANDSCAPE_BACKGROUND_IMAGE_NAME = "view_land.jpg";
+		
+		AuxiliaryView auxView = mock(AuxiliaryView.class);
+		when(auxView.getWidth()).thenReturn(WIDTH);
+		when(auxView.getHeight()).thenReturn(HEIGHT);
+		DownscalingBitmapLoader bitmapLoader = mock(DownscalingBitmapLoader.class);
+
+		CityConfiguration config = new CityConfiguration(createRandomString(), 
+				createRandomString(), EXPECTED_LANDSCAPE_BACKGROUND_IMAGE_NAME);
+		City city = new City(_random.nextInt(), createRandomString(), config);
+		
+		SutSetupResult sutSetupResult = setupSut(city, auxView, bitmapLoader);
+		
+		// --- Act
+		sutSetupResult.viewLayoutCompleteListener.onEvent();
+		
+		// --- Assert
+		verify(bitmapLoader).load(EXPECTED_LANDSCAPE_BACKGROUND_IMAGE_NAME, WIDTH, HEIGHT);
+	}
+	
+	private String createRandomString() {
+		return String.valueOf(_random.nextLong());
+	}
+	
+	private SutSetupResult setupSut(City city, AuxiliaryView view, 
+			DownscalingBitmapLoader bitmapLoader) {
+		SutSetupResult result = new SutSetupResult();
+
+		ArgumentCaptor<OnEventListener> viewLayoutCompleteListenerCaptor = 
+				ArgumentCaptor.forClass(OnEventListener.class);
+		doNothing().when(view).addViewLayoutCompleteListener(
+				viewLayoutCompleteListenerCaptor.capture());		
+		
+		AuxiliaryPresenter sut = new AuxiliaryPresenter(view, city);
+		sut.setBitmapLoader(bitmapLoader);
+		
+		result.sut = sut;
+		result.viewLayoutCompleteListener = viewLayoutCompleteListenerCaptor.getValue();
+		
+		return result;
+	}
+	
+	private class SutSetupResult {
+		public AuxiliaryPresenter sut;
+		public OnEventListener viewLayoutCompleteListener;
+	}
+}
