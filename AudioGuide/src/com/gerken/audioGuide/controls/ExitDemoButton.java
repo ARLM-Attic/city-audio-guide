eb740caa8647f03d91cc799e6565781b8c921088
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-12-05 11:21:01 +0000

    Styling the Exit Demo button
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@30107 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide/src/com/gerken/audioGuide/controls/ExitDemoButton.java
+++ AudioGuide/src/com/gerken/audioGuide/controls/ExitDemoButton.java
@@ -11,9 +11,11 @@ import android.widget.Button;
 
 public class ExitDemoButton extends Button {
 	private static final int DEF_COLOR = 0;
+	private static final float DEF_STROKE_WIDTH = 1.0f;
 	
 	private int _fillColor;
 	private int _strokeColor;
+	private float _strokeWidth;
 	
 	public ExitDemoButton(Context context, AttributeSet attrs) 
     { 
@@ -30,9 +32,14 @@ public class ExitDemoButton extends Button {
     private void init(Context context, AttributeSet attrs) {        
     	TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.ExitDemoButton);
     	_fillColor = arr.getColor(R.styleable.ExitDemoButton_fillColor, DEF_COLOR);
-    	_strokeColor = arr.getColor(R.styleable.ExitDemoButton_strokeColor, DEF_COLOR);    	
+    	_strokeColor = arr.getColor(R.styleable.ExitDemoButton_strokeColor, DEF_COLOR);
+    	_strokeWidth = arr.getDimension(R.styleable.ExitDemoButton_strokeWidth, DEF_STROKE_WIDTH);
     	arr.recycle();
     	
-    	setBackgroundDrawable(new ExitDemoButtonDrawable(new ExitDemoButtonShape(), _fillColor, _strokeColor));
+    	ExitDemoButtonDrawable d= new ExitDemoButtonDrawable(new ExitDemoButtonShape());
+    	d.setFillColor(_fillColor);
+    	d.setStrokeColor(_strokeColor);
+    	d.setStrokeWidth(_strokeWidth);
+    	setBackgroundDrawable(d);
     }
 }
--- AudioGuide/src/com/gerken/audioGuide/graphics/ExitDemoButtonDrawable.java
+++ AudioGuide/src/com/gerken/audioGuide/graphics/ExitDemoButtonDrawable.java
@@ -9,21 +9,22 @@ import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.Shape;
 
 public class ExitDemoButtonDrawable extends ShapeDrawable {
-	private int _fillColor;
-	private int _strokeColor;
+	private static final int DEF_COLOR = 0;
+	private static final float DEF_STROKE_WIDTH = 1.0f;	
+	
+	private int _fillColor = DEF_COLOR;
+	private int _strokeColor = DEF_COLOR;
 	private Shape _s;
 	
 	private Paint _fillPaint;
 	private Paint _strokePaint;
-	private Paint _crossPaint;
+	private Paint _crossPaint;	
 	
-	private static final float STROKE_WIDTH = 1.0f;
+	private float _strokeWidth = DEF_STROKE_WIDTH;
 	
-	public ExitDemoButtonDrawable(Shape s, int fillColor, int strokeColor) {
+	public ExitDemoButtonDrawable(Shape s) {
 		super(s);
 		_s = s;
-		_fillColor = fillColor;
-		_strokeColor = strokeColor;
 		
 		_fillPaint = new Paint(this.getPaint());
 		_fillPaint.setStyle(Style.FILL);
@@ -34,18 +35,35 @@ public class ExitDemoButtonDrawable extends ShapeDrawable {
 		_strokePaint.setStyle(Style.STROKE);
 		_strokePaint.setColor(_strokeColor);
 		_strokePaint.setAntiAlias(true);
-		_strokePaint.setStrokeWidth(STROKE_WIDTH);
+		_strokePaint.setStrokeWidth(_strokeWidth);
 		_strokePaint.setStrokeJoin(Paint.Join.ROUND);
 		_strokePaint.setStrokeCap(Paint.Cap.ROUND);
 		//_strokePaint.setMaskFilter(new BlurMaskFilter(1.0f, BlurMaskFilter.Blur.NORMAL));
 		
 		_crossPaint = new Paint(this.getPaint());
 		_crossPaint.setStyle(Style.STROKE);
-		_crossPaint.setColor(strokeColor);
+		_crossPaint.setColor(_strokeColor);
 		_crossPaint.setAntiAlias(true);
-		_crossPaint.setStrokeWidth(1.6f);
+		_crossPaint.setStrokeWidth(1.6f*_strokeWidth);
 		_crossPaint.setStrokeJoin(Paint.Join.ROUND);
 		_crossPaint.setStrokeCap(Paint.Cap.ROUND);
+	}
+	
+	public void setFillColor(int fillColor) {
+		_fillColor = fillColor;
+		_fillPaint.setColor(_fillColor);
+	}
+	
+	public void setStrokeColor(int strokeColor) {
+		_strokeColor = strokeColor;
+		_strokePaint.setColor(_strokeColor);
+		_crossPaint.setColor(_strokeColor);
+	}
+	
+	public void setStrokeWidth(float strokeWidth) {
+		_strokeWidth = strokeWidth;
+		_strokePaint.setStrokeWidth(_strokeWidth);
+		_crossPaint.setStrokeWidth(1.6f*_strokeWidth);
 	}
 	
 	@Override
@@ -53,7 +71,7 @@ public class ExitDemoButtonDrawable extends ShapeDrawable {
 		_s.resize(canvas.getClipBounds().right, canvas.getClipBounds().bottom);
 		_s.draw(canvas, _fillPaint);
 		
-		float strokeWidth = STROKE_WIDTH;
+		float strokeWidth = _strokeWidth;
 		Matrix matrix = new Matrix();
 	    matrix.setRectToRect(
 	    		new RectF(0, 0, canvas.getClipBounds().right, canvas.getClipBounds().bottom),
