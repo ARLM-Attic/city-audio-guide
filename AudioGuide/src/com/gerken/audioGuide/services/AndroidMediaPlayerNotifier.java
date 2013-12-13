decfeaac2ff97def1bc0745e7d53ab0511d0a853
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-12-13 17:01:58 +0000

    Fixed premature recycling of the MediaPlayer object
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@30358 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide/src/com/gerken/audioGuide/services/AndroidMediaPlayerNotifier.java
+++ AudioGuide/src/com/gerken/audioGuide/services/AndroidMediaPlayerNotifier.java
@@ -2,12 +2,22 @@ package com.gerken.audioGuide.services;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 
 import com.gerken.audioGuide.R;
 import com.gerken.audioGuide.interfaces.AudioNotifier;
 
 public class AndroidMediaPlayerNotifier implements AudioNotifier {
 	private Context _context;
+	private MediaPlayer _player;
+	
+	private OnCompletionListener _completionListener = new OnCompletionListener() {		
+		@Override
+		public void onCompletion(MediaPlayer mp) {
+			mp.release();
+			_player = null;
+		}
+	};
 	
 	public AndroidMediaPlayerNotifier(Context context) {
 		_context = context;
@@ -15,9 +25,9 @@ public class AndroidMediaPlayerNotifier implements AudioNotifier {
 	
 	@Override
 	public void signalSightInRange() {
-		MediaPlayer player = MediaPlayer.create(_context, R.raw.inrange);
-		player.start();
-		//player.release();		
+		_player = MediaPlayer.create(_context, R.raw.inrange);
+		_player.setOnCompletionListener(_completionListener);
+		_player.start();
 	}
 
 }
