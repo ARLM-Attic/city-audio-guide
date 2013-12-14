aaaede6c945fee895dc97e1718a0e555b471e272
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-12-14 17:05:17 +0000

    Creating player instance only once per object lifetime
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@30385 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- AudioGuide/src/com/gerken/audioGuide/services/AndroidMediaPlayerNotifier.java
+++ AudioGuide/src/com/gerken/audioGuide/services/AndroidMediaPlayerNotifier.java
@@ -2,7 +2,7 @@ package com.gerken.audioGuide.services;
 
 import android.content.Context;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnErrorListener;
 
 import com.gerken.audioGuide.R;
 import com.gerken.audioGuide.interfaces.AudioNotifier;
@@ -10,24 +10,29 @@ import com.gerken.audioGuide.interfaces.AudioNotifier;
 public class AndroidMediaPlayerNotifier implements AudioNotifier {
 	private Context _context;
 	private MediaPlayer _player;
-	
-	private OnCompletionListener _completionListener = new OnCompletionListener() {		
+
+	private OnErrorListener _errorListener = new OnErrorListener() {
+		
 		@Override
-		public void onCompletion(MediaPlayer mp) {
+		public boolean onError(MediaPlayer mp, int what, int extra) {
 			mp.release();
-			_player = null;
+			initPlayer();
+			return true;
 		}
 	};
 	
 	public AndroidMediaPlayerNotifier(Context context) {
 		_context = context;
+		initPlayer();
 	}
 	
 	@Override
 	public void signalSightInRange() {
-		_player = MediaPlayer.create(_context, R.raw.inrange);
-		_player.setOnCompletionListener(_completionListener);
 		_player.start();
 	}
-
+	
+	private void initPlayer() {
+		_player = MediaPlayer.create(_context, R.raw.inrange);
+		_player.setOnErrorListener(_errorListener);
+	}
 }
