4001eea13fe44324384bd78e8cb26732f8f35e46
SND\i_gerken_cp <SND\i_gerken_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2014-01-15 14:19:03 +0000

    Test added
    git-svn-id: https://cityaudioguide.svn.codeplex.com/svn@31319 3b72ae23-dab2-410c-8069-39ac8a3268c2

---
+++ AudioGuide.UnitTests/src/com/gerken/audioGuideTests/presenters/routeMapPresenter/HandleMultiTouchMove.java
@@ -0,0 +1,64 @@
+package com.gerken.audioGuideTests.presenters.routeMapPresenter;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.mockito.AdditionalMatchers;
+import org.mockito.ArgumentCaptor;
+
+import com.gerken.audioGuide.containers.Point;
+import com.gerken.audioGuide.interfaces.LocationTracker;
+import com.gerken.audioGuide.interfaces.MediaAssetManager;
+import com.gerken.audioGuide.interfaces.OnLocationChangedListener;
+import com.gerken.audioGuide.interfaces.OnMultiTouchListener;
+import com.gerken.audioGuide.interfaces.views.RouteMapView;
+import com.gerken.audioGuide.objectModel.City;
+import com.gerken.audioGuide.presenters.RouteMapPresenter;
+
+public class HandleMultiTouchMove {
+	private static final float DEFAULT_DELTA = 0.001f; 
+
+	@Test
+	public void Given_ZoomOutMoveDone__Then_MapScaleSet_MapPointerScaleSet() {
+		final float EXPECTED_SCALE = 0.5f;
+		
+		RouteMapView view = mock(RouteMapView.class);
+		when(view.getHeight()).thenReturn(40);
+		when(view.getWidth()).thenReturn(40);
+		when(view.getOriginalMapWidth()).thenReturn(160);
+		when(view.getOriginalMapHeight()).thenReturn(160);
+		
+		SutSetupResult sutSetupResult = setupSut(view);
+		sutSetupResult.multiTouchListener.onMultiTouchDown(
+			(Point<Float>[])new Point[]{ new Point<Float>(20f, 0f), new Point<Float>(40f, 0f) }
+		);
+		
+		// --- Act
+		sutSetupResult.multiTouchListener.onMultiTouchMove(
+			(Point<Float>[])new Point[]{ new Point<Float>(25f, 0f), new Point<Float>(35f, 0f) }
+		);
+		
+		// --- Assert
+		verify(view).setMapScale(AdditionalMatchers.eq(EXPECTED_SCALE, DEFAULT_DELTA));
+		verify(view).setMapPointerScale(AdditionalMatchers.eq(EXPECTED_SCALE, DEFAULT_DELTA));
+	}
+	
+	private SutSetupResult setupSut(RouteMapView view) {
+		SutSetupResult result = new SutSetupResult();
+		
+		ArgumentCaptor<OnMultiTouchListener> multiTouchListenerCaptor = 
+				ArgumentCaptor.forClass(OnMultiTouchListener.class);
+		doNothing().when(view).addViewMultiTouchListener(multiTouchListenerCaptor.capture());
+		
+		City city = new City();
+		result.sut = new RouteMapPresenter(city, view, mock(MediaAssetManager.class));
+		result.multiTouchListener = multiTouchListenerCaptor.getValue();
+		
+		return result;
+	}
+	
+	private class SutSetupResult {
+		public RouteMapPresenter sut;
+		public OnMultiTouchListener multiTouchListener;
+	}
+}
